e093d6e2312e0e2c5ecd5ad7e2cfbc7c45679013
Xingping Ding <xingpingding@gmail.com>
2018-06-04 08:15:26 +1000

    view tests
    view tests

---
+++ parking/Views/PKTestView.swift
@@ -0,0 +1,21 @@
+//
+//  PKTestView.swift
+//  parking
+//
+//  Created by Xingping Ding on 2018/6/4.
+//  Copyright © 2018年 Xingping Ding. All rights reserved.
+//
+
+import UIKit
+
+class PKTestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
